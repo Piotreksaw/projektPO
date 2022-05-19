c2f584c321038ce625631deef0cf1871d3dc4a2c
Piotr Sawicki <piotrsawicki13@gmail.com>
2022-05-19 22:00:43 +0200

    zmiany

--- glowne_pliki/main_window.py
+++ glowne_pliki/main_window.py
@@ -1,7 +1,8 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QGridLayout, QGroupBox, QLineEdit
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QGridLayout, QGroupBox, QLineEdit, QTabWidget
 # from PyQt5 import QtCore
 from buttons import Map_and_Chart_buttons, Export_and_something_buttons
+from PyQt5. QtCore import pyqtSlot
 
 
 class MainWindow(QWidget):
@@ -13,10 +14,11 @@ class MainWindow(QWidget):
 
         self.resize(width, height)
         self.setWindowTitle(name)
+        # self.tab_widget = Tabs(self)
         self.__prepare_window()
 
     def __prepare_window(self):
-        self.__buttons1 = Map_and_Chart_buttons()
+        self.__buttons1 = Tabs(self)
         self.__buttons2 = Export_and_something_buttons()
         # self.__map = Map
         # self.__chart = Chart
@@ -30,8 +32,23 @@ class MainWindow(QWidget):
         self.setLayout(main_layout)
         self.show()
 
+class Tabs(QWidget):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        self.layout = QVBoxLayout(self)
+        self.tabs = QTabWidget()
+        self.tab1 = QWidget()
+        self.tab2 = QWidget()
+        self.tabs.resize(50, 50)
+        self.tabs.addTab(self.tab1, "Tab 1")
+        self.tabs.addTab(self.tab2, "Tab 2")
+        self.tab1.layout = QVBoxLayout(self)
+        self.layout.addWidget(self.tabs)
+        self.setLayout(self.layout)
 
 if __name__ == "__main__":
     app = QApplication([])
     main_window = MainWindow()
+
     sys.exit(app.exec_())
