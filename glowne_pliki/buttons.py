b64ce650df962f96c4a257741cb74b18e0bd047f
Piotr Sawicki <piotrsawicki13@gmail.com>
2022-05-21 13:20:40 +0200

    nawaliłem nowych elementów ale żaden nie dziala :(((

--- glowne_pliki/buttons.py
+++ glowne_pliki/buttons.py
@@ -7,35 +7,35 @@ class create_button(QPushButton):
 
 
 
-class Map_button(create_button):
-    def __init__(self):
-        super().__init__(word = "mapa")
-        
-
-
-class Chart_button(create_button):
-    def __init__(self):
-        super().__init__("chart")
-
-
-class Map_and_Chart_buttons(QGroupBox):
-    def __init__(self):
-        super().__init__()
-        # self.__text_panel = text_panel
-        self.__prepare_buttons()
-
-    def __prepare_buttons(self):
-        self.__create_text_button()
-        layout = QGridLayout()
-
-        layout.addWidget(self.__button1, 0, 0)
-        layout.addWidget(self.__button2, 0, 1)
-
-        self.setLayout(layout)
-
-    def __create_text_button(self):
-        self.__button1 = Map_button()
-        self.__button2 = Chart_button()
+# class Map_button(create_button):
+#     def __init__(self):
+#         super().__init__(word = "mapa")
+#
+#
+#
+# class Chart_button(create_button):
+#     def __init__(self):
+#         super().__init__("chart")
+#
+#
+# class Map_and_Chart_buttons(QGroupBox):
+#     def __init__(self):
+#         super().__init__()
+#         # self.__text_panel = text_panel
+#         self.__prepare_buttons()
+#
+#     def __prepare_buttons(self):
+#         self.__create_text_button()
+#         layout = QGridLayout()
+#
+#         layout.addWidget(self.__button1, 0, 0)
+#         layout.addWidget(self.__button2, 0, 1)
+#
+#         self.setLayout(layout)
+#
+#     def __create_text_button(self):
+#         self.__button1 = Map_button()
+#         self.__button2 = Chart_button()
 
 
 class Export_button(create_button):
--- glowne_pliki/main_window.py
+++ glowne_pliki/main_window.py
@@ -1,8 +1,11 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QGridLayout, QGroupBox, QLineEdit, QTabWidget
 # from PyQt5 import QtCore
-from buttons import Map_and_Chart_buttons, Export_and_something_buttons
+from buttons import Export_and_something_buttons
 from PyQt5. QtCore import pyqtSlot
+from tabs import Tabs
+from list_of_countries import ButtonsPanel
+
 
 
 class MainWindow(QWidget):
@@ -18,11 +21,12 @@ class MainWindow(QWidget):
         self.__prepare_window()
 
     def __prepare_window(self):
-        self.__buttons1 = Tabs(self)
+        self.__buttons1 = Tabs()
         self.__buttons2 = Export_and_something_buttons()
         # self.__map = Map
-        # self.__chart = Chart
-        # self.__list = List
+        # self.__chart =
+        # self.__list = LisCt
+        self.__button_panel = ButtonsPanel()
 
         main_layout = QGridLayout()
         main_layout.addWidget(self.__buttons1, 0, 0)
@@ -32,20 +36,7 @@ class MainWindow(QWidget):
         self.setLayout(main_layout)
         self.show()
 
-class Tabs(QWidget):
-    def __init__(self, parent):
-        super().__init__(parent)
 
-        self.layout = QVBoxLayout(self)
-        self.tabs = QTabWidget()
-        self.tab1 = QWidget()
-        self.tab2 = QWidget()
-        self.tabs.resize(50, 50)
-        self.tabs.addTab(self.tab1, "Wykres")
-        self.tabs.addTab(self.tab2, "Mapa")
-        self.tab1.layout = QVBoxLayout(self)
-        self.layout.addWidget(self.tabs)
-        self.setLayout(self.layout)
 
 if __name__ == "__main__":
     app = QApplication([])
