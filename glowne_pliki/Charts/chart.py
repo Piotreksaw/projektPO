49cceb46a2b7202b7e90af8ff374db6bb4f40e91
Piotr Sawicki <piotrsawicki13@gmail.com>
2022-05-24 11:33:45 +0200

    troche małych zmian

--- glowne_pliki/Charts/chart.py
+++ glowne_pliki/Charts/chart.py
@@ -5,8 +5,10 @@ from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
 from matplotlib.figure import Figure
 from PyQt5.QtGui import QIcon, QPixmap
 
+from projektPO.glowne_pliki.FileReader import FileReader, Country
 
-class create_Chart(FigureCanvasQTAgg):
+
+class CreateChart(FigureCanvasQTAgg):
     def __init__(self, width = 5, height = 5, dpi = 100):
         self.__fig = Figure(figsize=(width, height), dpi= dpi)
         super().__init__(self.__fig)
@@ -26,3 +28,4 @@ class create_Chart(FigureCanvasQTAgg):
         self.__axes.plot(xx, yy, color, label= name)
         self.__axes.legend()
         self.draw()
+
