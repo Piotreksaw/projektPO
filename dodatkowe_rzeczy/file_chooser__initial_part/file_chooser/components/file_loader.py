961fdee98112f95575900b596b13c7133565e6ea
Oliwier <oliwierkniazewski@gmail.com>
2022-05-28 16:29:19 +0200

    dodanie przycisku do otwarcia pliku csv, zakomentowalem slidery bo generowaly bledy

--- dodatkowe_rzeczy/file_chooser__initial_part/file_chooser/components/file_loader.py
+++ dodatkowe_rzeczy/file_chooser__initial_part/file_chooser/components/file_loader.py
@@ -3,22 +3,20 @@ import sys
 
 from PyQt5.QtWidgets import QPushButton, QProgressBar, QFileDialog, QHBoxLayout
 
-from  projektPO.dodatkowe_rzeczy.file_chooser__initial_part.file_chooser.components.io import FileReader
+from projektPO.dodatkowe_rzeczy.file_chooser__initial_part.file_chooser.components.io import FileReader
 
 
 class FileLoader(QHBoxLayout):
 
     def __init__(self, btn_name):
         super().__init__()
-
+        self.__selected_filepath = 'none'
         self.__create_all(btn_name)
 
     def __create_all(self, btn_name, parent=None):
         self.__file_loader_dialog_btn = self.__create_file_loader_dialog_btn(btn_name)
-        self.__loader = QProgressBar(parent)
 
         self.addWidget(self.__file_loader_dialog_btn)
-        self.addWidget(self.__loader)
 
     def __create_file_loader_dialog_btn(self, btn_name):
         loader_btn = QPushButton(btn_name)
@@ -27,22 +25,15 @@ class FileLoader(QHBoxLayout):
         return loader_btn
 
     def __choose_and_read_file(self):
-        self.__loader.setValue(1)
         parent = None
         current_dir = os.path.dirname(sys.argv[0])
-        self.__loader.setValue(10)
         options = QFileDialog.DontUseNativeDialog
-        self.__loader.setValue(20)
         maybe_selected_file, _ = QFileDialog.getOpenFileName(parent, "Choose csv file",
                                                              current_dir, "CSV (*.csv);;All Files (*)",
                                                              options=options)
-        self.__loader.setValue(40)
-        print(maybe_selected_file)
         if maybe_selected_file:
-            self.__loader.setValue(50)
-            file_reader = FileReader(maybe_selected_file)
-            # file_reader.read()
-            self.__loader.setValue(100)
+            f = open("filepath.txt", "w+")
+            f.write(maybe_selected_file)
+            f.close()
         else:
-            print("sth wrong???")
-            self.__loader.setValue(1)
+            print("does not work")
--- glowne_pliki/buttons.py
+++ glowne_pliki/buttons.py
@@ -1,5 +1,6 @@
 from PyQt5.QtWidgets import QPushButton, QGroupBox, QGridLayout
 from text_panel import TextPanel as TP
+from dodatkowe_rzeczy.file_chooser__initial_part.file_chooser.components.file_loader import FileLoader
 
 
 class create_button(QPushButton):
@@ -12,10 +13,6 @@ class Export_to_pdf_button(create_button):
         super().__init__("Export to PDF")
 
 
-class Something_button(create_button):
-    def __init__(self):
-        super().__init__("insert file")
-
 class Export_and_something_buttons(QGroupBox):
     def __init__(self):
         super().__init__()
@@ -25,8 +22,7 @@ class Export_and_something_buttons(QGroupBox):
     def __prepare_buttons(self):
         self.__create_text_button()
         layout = QGridLayout()
-
-        layout.addWidget(self.__button2, 0, 0, 1, 1)
+        layout.addLayout(self.__file_loader_button, 0, 0, 1, 1)
         layout.addWidget(self.__button1, 0, 1, 1, 1)
         layout.addWidget(self.__text_panel, 0, 2, 1, 2)
 
@@ -34,5 +30,5 @@ class Export_and_something_buttons(QGroupBox):
 
     def __create_text_button(self):
         self.__button1 = Export_to_pdf_button()
-        self.__button2 = Something_button()
+        self.__file_loader_button = FileLoader("Select file")
         self.__text_panel = TP()
